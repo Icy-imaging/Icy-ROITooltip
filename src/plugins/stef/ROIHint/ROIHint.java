82e6aa6c6f8cf107a5e069dff7a15884ebf9a501
Stephane <stephane.dallongeville@pasteur.fr>
2012-03-08 18:25:37 +0100

    first commit.

---
+++ src/plugins/stef/ROIHint/ROIHint.java
@@ -0,0 +1,14 @@
+package plugins.stef.ROIHint;
+
+import icy.plugin.abstract_.Plugin;
+
+/**
+ * ROI Hint plugin.<br>
+ * Add hint on focused ROI in Canvas2D.
+ * 
+ * @author Stephane
+ */
+public class ROIHint extends Plugin
+{
+
+}

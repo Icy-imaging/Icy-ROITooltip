d0926e973da14d5aa67d8b57670948b300c8f800
Stephane <stephane.dallongeville@pasteur.fr>
2012-10-29 17:58:45 +0100

    updated for last kernel.

--- src/plugins/stef/roi/ROIToolTip.java
+++ src/plugins/stef/roi/ROIToolTip.java
@@ -3,18 +3,18 @@ package plugins.stef.roi;
 import icy.canvas.IcyCanvas;
 import icy.canvas.IcyCanvas2D;
 import icy.gui.main.FocusedSequenceListener;
+import icy.image.IntensityInfo;
 import icy.main.Icy;
 import icy.painter.AbstractPainter;
 import icy.plugin.abstract_.Plugin;
 import icy.plugin.interface_.PluginDaemon;
 import icy.roi.ROI;
 import icy.roi.ROI2D;
-import icy.roi.ROI3D;
 import icy.roi.ROIEvent;
 import icy.roi.ROIEvent.ROIEventType;
 import icy.roi.ROIListener;
+import icy.roi.ROIUtil;
 import icy.sequence.Sequence;
-import icy.sequence.SequenceDataIterator;
 import icy.sequence.SequenceEvent;
 import icy.system.thread.SingleProcessor;
 import icy.util.GraphicsUtil;
@@ -37,20 +37,6 @@ import java.awt.geom.Rectangle2D;
  */
 public class ROIToolTip extends Plugin implements PluginDaemon, FocusedSequenceListener, ROIListener
 {
-    private static class ROIExtraInfos
-    {
-        public long numPixels;
-        public double area;
-        public double minIntensity;
-        public double maxIntensity;
-        public double meanIntensity;
-
-        public ROIExtraInfos()
-        {
-            super();
-        }
-    }
-
     private class ROICalculator implements Runnable
     {
         public ROICalculator()
@@ -61,74 +47,16 @@ public class ROIToolTip extends Plugin implements PluginDaemon, FocusedSequenceL
         @Override
         public void run()
         {
-            try
+            final Sequence seq = focusedSequence;
+            final ROI roi = focusedROI;
+
+            if ((seq != null) && (roi != null))
             {
-                if ((focusedSequence != null) && (focusedROI != null))
-                {
-                    final double psx = focusedSequence.getPixelSizeX();
-                    final double psy = focusedSequence.getPixelSizeY();
-                    final double psz = focusedSequence.getPixelSizeZ();
-                    final double tot;
-
-                    // preliminary stuff to display pixel surface
-                    if (focusedROI instanceof ROI3D)
-                        tot = psx * psy * psz;
-                    else
-                        tot = psx * psy;
-
-                    final SequenceDataIterator it = new SequenceDataIterator(focusedSequence, focusedROI);
-
-                    long numPixels = 0;
-                    double min = Double.MAX_VALUE;
-                    double max = Double.MIN_VALUE;
-                    double sum = 0;
-
-                    // faster to do all calculation in a single iteration run
-                    while (!it.done())
-                    {
-                        // verify if we can abort calculation...
-                        if (((numPixels & 0xFFFF) == 0) && processor.hasWaitingTasks())
-                            return;
-
-                        final double value = it.get();
-                        it.next();
-
-                        if (value < min)
-                            min = value;
-                        if (value > max)
-                            max = value;
-                        sum += value;
-                        numPixels++;
-                    }
-
-                    if (numPixels > 0)
-                    {
-                        roiInfos.area = numPixels * tot;
-                        roiInfos.minIntensity = min;
-                        roiInfos.maxIntensity = max;
-                        roiInfos.meanIntensity = sum / numPixels;
-                    }
-                    else
-                    {
-                        roiInfos.area = 0d;
-                        roiInfos.minIntensity = 0d;
-                        roiInfos.maxIntensity = 0d;
-                        roiInfos.meanIntensity = 0d;
-                    }
-                    roiInfos.numPixels = numPixels;
-                }
-                else
-                {
-
-                }
-            }
-            catch (Exception e)
-            {
-                // we can have exception as the process can be really long
-                // and size modified during this period
+                intensityInfo = ROIUtil.getIntensityInfo(focusedSequence, focusedROI);
+                perimeter = roi.getPerimeter();
+                volume = roi.getVolume();
             }
 
-            // calculation done, notify painter changed
             painter.changed();
         }
     }
@@ -181,13 +109,13 @@ public class ROIToolTip extends Plugin implements PluginDaemon, FocusedSequenceL
                                 + StringUtil.toString(bounds.getHeight(), 1);
                         text += "\n";
 
-                        text += "Pixels: " + StringUtil.toString(roiInfos.numPixels) + updatingMark;
-                        text += "  Surface: " + StringUtil.toString(roiInfos.area, 1) + updatingMark;
+                        text += "Perimeter: " + StringUtil.toString(perimeter) + updatingMark;
+                        text += "  Volume: " + StringUtil.toString(volume, 1) + updatingMark;
                         text += "\n";
                         text += "Intensity";
-                        text += "  min: " + StringUtil.toString(roiInfos.minIntensity, 1) + updatingMark;
-                        text += "  max: " + StringUtil.toString(roiInfos.maxIntensity, 1) + updatingMark;
-                        text += "  mean: " + StringUtil.toString(roiInfos.meanIntensity, 1) + updatingMark;
+                        text += "  min: " + StringUtil.toString(intensityInfo.minIntensity, 1) + updatingMark;
+                        text += "  max: " + StringUtil.toString(intensityInfo.maxIntensity, 1) + updatingMark;
+                        text += "  mean: " + StringUtil.toString(intensityInfo.meanIntensity, 1) + updatingMark;
 
                         final Graphics2D g2 = (Graphics2D) g.create();
 
@@ -228,7 +156,13 @@ public class ROIToolTip extends Plugin implements PluginDaemon, FocusedSequenceL
 
     final HintPainter painter;
     final SingleProcessor processor;
-    final ROIExtraInfos roiInfos;
+
+    /**
+     * ROI calculated infos
+     */
+    IntensityInfo intensityInfo;
+    double perimeter;
+    double volume;
 
     public ROIToolTip()
     {
@@ -236,7 +170,7 @@ public class ROIToolTip extends Plugin implements PluginDaemon, FocusedSequenceL
 
         painter = new HintPainter();
         processor = new SingleProcessor(true);
-        roiInfos = new ROIExtraInfos();
+        intensityInfo = new IntensityInfo();
     }
 
     void updateInfos()
@@ -245,14 +179,21 @@ public class ROIToolTip extends Plugin implements PluginDaemon, FocusedSequenceL
     }
 
     @Override
-    public void run()
+    public void init()
     {
         focusedSequence = null;
         focusedROI = null;
+
         sequenceFocused(getFocusedSequence());
         roiFocused((focusedSequence != null) ? focusedSequence.getFocusedROI() : null);
 
         Icy.getMainInterface().addFocusedSequenceListener(this);
+    }
+
+    @Override
+    public void run()
+    {
+        // nothing to do here
     }
 
     @Override
